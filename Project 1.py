c6110a9d24239958e0807ca573d7577536b2f319
johnjackhohl <johnjackhohl@gmail.com>
2022-09-12 14:13:16 -0400

    Co-authored-by: Matthew Mercado <Stixels@users.noreply.github.com>

--- Project 1.py
+++ Project 1.py
@@ -1,26 +1,46 @@
 def main():
-    # starting dice roll
-    start = input("Please enter a 4 digit dice roll:")
-    # starting dice roll into array 
-    dice = [int(x) for x in str(start)]
-    # end dice roll
-    end = input("Please enter what the previous dice roll should turn too: ")
-    # end dice roll into array
-    endDice = [int(x) for x in str(end)]
-# adds one to one dice
-def bump(dice, poss):
-    if dice[poss] != 6:
-        dice[poss]+=1
-    return dice
-# flips once dice to the opposite side
-def flip(dice, poss):
-    dice[poss] = 7 - dice[poss]
-    return dice
-# swaps two dice's possition
-def swap(dice, poss1, poss2):
-    temp = dice[poss1]
-    dice[poss1] = dice[poss2]
-    dice[poss2] = temp
+    # start = input("Please enter a 4 digit dice roll:")
+    startDiceRolls = "1234"
+    startDice = [int(x) for x in str(startDiceRolls)]
+    
+    # end = input("Please enter what the previous dice roll should turn too: ")
+    endDiceRolls = "4234"
+    
+    endDice = [int(x) for x in str(endDiceRolls)]
+    print(bfs(startDice, endDice))
+    
+    
+def bump(dice, pos):
+    if dice[pos] >= 1 and dice[pos] <= 5:
+        dice[pos]+=1
     return dice
 
-main()
+def flip(dice, pos):
+    dice[pos] = 7 - dice[pos]
+    return dice
+
+def swap(dice, pos1, pos2):
+    temp = dice[pos1]
+    dice[pos1] = dice[pos2]
+    dice[pos2] = temp
+    return dice
+
+def bfs(start, end):
+    queue = [start]
+    visited = []
+    while queue:
+        dice = queue.pop(0)
+        if dice == end:
+            return visited
+        if dice not in visited:
+            visited.append(dice)
+            for i in range(4):
+                queue.append(bump(dice[:], i))
+                queue.append(flip(dice[:], i))
+                for j in range(4):
+                    if i != j:
+                        queue.append(swap(dice[:], i, j))
+    return None
+    
+if __name__ == "__main__":
+    main()
